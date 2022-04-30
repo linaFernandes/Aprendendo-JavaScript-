c2871d62864c13259a0225a3b9cd6327653b214b
linaFernandes <calina.marx@gmail.com>
2022-04-30 00:00:06 -0300

    Inclusão dos arquivos do curso de JavaScript

---
+++ Primeiros códigos/index.js
@@ -0,0 +1,12 @@
+
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' '+ lastName);
+}
+
+greet('Lina', 'Fernandes');
+
+function square(number) {
+    return number * number;
+}
+
+console.log(square(6));
